90903c0b0c9bde54e9f7190dd7751a5d63d11b7f
Bello Shukroh <adetejubello@gmail.com>
2022-09-13 20:58:18 +0000

    tasks

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" Define a class Square"""
+"""Define a class Square"""
 
 
 class Square:
-    """ This class defines a square
-        Private instance attribute: size
-        Instantiation with size (no type/value verification)
+    """This class defines a square
+    Private instance attribute: size
+    Instantiation with size (no type/value verification)
     """
 
     def __init__(self, size):
