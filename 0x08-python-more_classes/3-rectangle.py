4a6db77a4c164f3bc74d665cdd7c970a1196de13
Bello Shukroh <adetejubello@gmail.com>
2022-09-19 15:23:41 +0000

    Task 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -53,9 +53,10 @@ class Rectangle:
         """Prints the character #"""
         if self.__width == 0 or self._height == 0:
             return ""
-        rect = ""
+
+        rect = []
         for i in range(self.__height):
-            for j in range(self.__width):
-                rect += "#"
-            rect += "\n"
-        return rect[:-1]
+            [rect.append("#") for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
