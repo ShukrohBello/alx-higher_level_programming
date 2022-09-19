f7ad5427ef4ca52dbc7714dd61163ec90ee742ae
Bello Shukroh <adetejubello@gmail.com>
2022-09-19 17:41:51 +0000

    task 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -5,16 +5,16 @@
 class Rectangle:
     """Represents a Rectange
 
-    Defining an attribute"""
-
+    Defining an attribute
+    """
 
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """Initialize the data"""
+        Rectangle.number_of_instances += 1
         self.width = width
         self.height = height
-        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
