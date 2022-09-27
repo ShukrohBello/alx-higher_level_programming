46bcc4e8ba3b4a1da897bc435d21db2f4e7e80ef
Bello Shukroh <adetejubello@gmail.com>
2022-09-27 22:11:04 +0000

    tasks 0-2

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=""):
     """Reads from filename and prints to stdout"""
     with open(filename, encoding="utf-8") as f:
-        printf(f.read(), end="")
+        read_text = f.read()
+        print(read_text, end="")
